4d7532e57348188b1c9821aa58108358fd52580d
twincacca <twincacca@users.noreply.github.com>
2024-05-15 12:20:23 +0200

    Update open_infra_pr.sh

--- scripts/open_infra_pr.sh
+++ scripts/open_infra_pr.sh
@@ -4,3 +4,7 @@ BRANCH_NAME=gitops-demo-bot/$TAG
 
 git clone https://$GH_TOKEN@github.com/cicd-excellence/infra.git
 
+cd infra
+
+git config --global user.email ""
+git config --global user.name "GitOps Demo Bot"
