20e1adee8356722430bc9768c36d44485887656d
twincacca <twincacca@users.noreply.github.com>
2024-05-15 12:21:51 +0200

    Update open_infra_pr.sh

--- scripts/open_infra_pr.sh
+++ scripts/open_infra_pr.sh
@@ -8,3 +8,19 @@ cd infra
 
 git config --global user.email ""
 git config --global user.name "GitOps Demo Bot"
+
+git checkout -b $BRANCH_NAME
+
+TAG=$TAG make dev.update
+
+git add .
+git commit -m "Update dev to $TAG"
+git push origin $BRANCH_NAME
+
+gh pr create \
+  --body "" \
+  --title "Update dev to $TAG" \
+  --head "$BRANCH_NAME" \
+  --base "main"
+
+gh pr merge --admin --rebase
